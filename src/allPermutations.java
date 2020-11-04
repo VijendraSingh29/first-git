7f1b730d1b04fcdace359073846f01ea916848f2
VJ <vijendra.jadaun@gmail.com>
2020-11-04 23:47:37 +0530

    optimize with reducing number of call for allpermu() function.

--- src/allPermutations.java
+++ src/allPermutations.java
@@ -2,55 +2,46 @@ import java.util.HashSet;
 
 public class allPermutations {
 
-    public static void main(String [] args)
-    {
+    // https://leetcode.com/problems/letter-tile-possibilities/
 
-        System.out.println("hello permu");
-        String s = "AAB" ;
-        HashSet<String > set = new HashSet<>() ;
-        set.add("ABC") ;
-        //     ArrayList<Character> list = new ArrayList<>() ;
+    public static void main(String[] args) {
+        String s = "AAAB";
+        HashSet<String> set = new HashSet<>();
+        StringBuilder sb = new StringBuilder();
+        allPermutations p = new allPermutations();
 
-        StringBuilder sb = new StringBuilder() ;
-        allPermutations p =new allPermutations();
-
-        for(int k=1;k<=s.length() ;k++)
-            p.allpermu(s,0,k,sb,set);
-
+        p.allpermu(s, 0, 1, sb, set);
+        System.out.println(set.size()); //total number of possibility
     }
-    public static void allpermu(String s, int start,int k,StringBuilder sb,HashSet<String> set)
-    {
-        if(sb.length() == k)
-        {
-            if(!set.contains(sb.toString())) {
+
+    public static void allpermu(String s, int start, int k, StringBuilder sb, HashSet<String> set) {
+        if (sb.length() > 0) {  // for each possibility of sequence ,length of 1,2,... n.
+            if (!set.contains(sb.toString())) {
                 System.out.println(sb.toString());
-                // String ss= temp.toString() ;
-                //  System.out.println(ss);
                 set.add(sb.toString());
             }
-            return;
         }
-        for(int i=start ;i<3;i++)
-        {  s= swap(s,i,start);
+        if (start == s.length())
+            return;
+        for (int i = start; i < s.length(); i++) {
+            s = swap(s, i, start);
             sb.append(s.charAt(start));
-            if(!set.contains(s)) {
-                allpermu(s, start + 1, k, sb, set);
-            }
-            sb.deleteCharAt(sb.length()-1);
-
-            s= swap(s,i,start) ;
+            allpermu(s, start + 1, k, sb, set);
+            //backtracking
+            sb.deleteCharAt(sb.length() - 1);
+            s = swap(s, i, start);
         }
 
 
     }
 
-    private static  String  swap(String s, int i, int j) {
+    private static String swap(String s, int i, int j) {
         char temp;
         char[] charArray = s.toCharArray();
-        temp = charArray[i] ;
+        temp = charArray[i];
         charArray[i] = charArray[j];
-        charArray[j] = temp ;
-        return  String.valueOf(charArray) ;
+        charArray[j] = temp;
+        return String.valueOf(charArray);
     }
 
 }
